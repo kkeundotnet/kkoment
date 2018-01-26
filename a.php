2f22e64a3be7b9cf51e64356aacd54e64bd7624d
kkeundotnet <kkeundotnet@gmail.com>
2018-01-26 17:13:31 +0900

    Print removed message

--- a.php
+++ a.php
@@ -15,7 +15,7 @@ $thread_id = $_REQUEST['thread_id'];
 // get data
 $db = new SQLite3('_db/all');
 $stmt = $db->prepare(
-'SELECT id, name, time, hashed, text FROM comments
+'SELECT id, name, time, hashed, text, removed FROM comments
  WHERE url=:url AND thread_id=:thread_id');
 $stmt->bindParam(':url', $url);
 $stmt->bindParam(':thread_id', $thread_id);
@@ -23,6 +23,9 @@ $result = $stmt->execute();
 
 $all = array();
 while($row = $result->fetchArray()) {
+    if($row['removed']) {
+        $row['text'] = "<p style=\"color:red;\">관리자에 의해 삭제된 메세지입니다.</p>";
+    }
     array_push($all, $row);
 }
 echo (json_encode($all));
